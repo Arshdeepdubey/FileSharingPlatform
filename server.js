eb9ffa08e243e7dccc617edcf8856ad89325976e
arshdeepdubey.ad <53818463+Arshdeepdubey@users.noreply.github.com>
2024-09-12 12:57:10 +0530

    feat(Arsh): Simple platform build in which we share file with password encryption

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const multer = require("multer");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const File = require("./models/File")
+const File = require("./models/File");
 
 const express = require("express");
 // const req = require("express/lib/request");
 const app = express();
+app.use(express.urlencoded({ extended: true }))
 
 const upload = multer({ dest: "uploads" });
 
@@ -25,12 +26,45 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     originalName: req.file.originalname,
   };
   if (req.body.password != null && req.body.password !== "") {
-    fileData.password = bcrypt.hash(req.body.password, 10);
+    fileData.password = await bcrypt.hash(req.body.password, 10);
   }
 
   const file = await File.create(fileData);
-  console.log(file);
-  res.send(file.originalName);
+
+  // console.log(file);
+  // res.send(file.originalName);
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
+
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+// app.get("/file/:id", handleDownload)
+// app.get("/file/:id", handleDownload)
+
+// res.send(req.params.id);
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return;
+    }
+
+    // if (checkPassword)
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true })
+      return
+    }
+  }
+
+  file.downloadCount++;
+  await file.save();
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName);
+}
 
 app.listen(process.env.PORT);
